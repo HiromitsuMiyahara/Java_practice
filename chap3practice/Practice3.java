06f1240d31e0e828c23266bf64600fc0017c2620
HiromitsuMiyahara <0912.hiromi.0912@gmail.com>
2024-09-08 13:13:52 +0000

    練習問題3(三項条件演算子にした場合)

--- chap3practice/Practice3.java
+++ chap3practice/Practice3.java
@@ -1,14 +1,18 @@
 public class Practice3 {
   public static void main(String[] args) {
-    int isHungry = 1;
+    int isHungry = 0;
     String food = "ハンバーガー";
     System.out.println("こんにちは");
-    if (isHungry == 0) {
-      System.out.println("お腹がいっぱいです");
-    } else {
-      System.out.println("腹ペコです");
+    System.out.println(isHungry == 0 ? "お腹がいっぱいです" : "はらぺこです");
+    if (isHungry == 1) {
       System.out.println(food + "をいただきます");
     }
+    // if (isHungry == 0) {
+    //   System.out.println("お腹がいっぱいです");
+    // } else {
+    //   System.out.println("腹ペコです");
+    //   System.out.println(food + "をいただきます");
+    // }
     System.out.println("ごちそうさまでした");
   }
 }
