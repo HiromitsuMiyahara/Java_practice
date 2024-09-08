2652ac4ebf3d3b10d10a07daf062af050f8c43c2
HiromitsuMiyahara <0912.hiromi.0912@gmail.com>
2024-09-08 10:20:26 +0000

    練習問題4

---
+++ chap3practice/Practice4.java
@@ -0,0 +1,12 @@
+public class Practice4 {
+  public static void main(String[] args) {
+    boolean tenki = true;
+    if (tenki == false) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩にいきます");
+    } else {
+      System.out.println("DVDを見ます");
+      System.out.println("寝ます");
+    }
+  }
+}
