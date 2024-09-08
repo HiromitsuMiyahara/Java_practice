39687a6371c17f25ec9551c5d1d49412139f4296
HiromitsuMiyahara <0912.hiromi.0912@gmail.com>
2024-09-08 09:57:29 +0000

    練習問題1

---
+++ chap3practice/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    String name = "湊";
+    if (name.equals("奏")) {
+      System.out.println("名前は奏である");
+    } else {
+      System.out.println("folse");
+    }
+  }
+}
