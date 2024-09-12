5d2227325261779f676c83645f27002267360992
HiromitsuMiyahara <0912.hiromi.0912@gmail.com>
2024-09-12 13:15:34 +0000

    メソッドの呼び出し

--- 5-2/Main.java
+++ 5-2/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-  public static {
-    
+  public static void main(String[] args) {
+    System.out.println("メソッドを呼び出します");
+    hello();
+    System.out.println("メソッドの呼び出しが終わりました");
+  }
+  public static void helli(); {
+    System.out.println("湊さん、こんにちは");
   }
 }
