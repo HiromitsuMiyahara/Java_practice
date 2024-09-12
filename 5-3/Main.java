28effe9c693a3a19e5281cc4ccd2e399850dd6ab
HiromitsuMiyahara <0912.hiromi.0912@gmail.com>
2024-09-12 14:42:11 +0000

    mainメソッド以外からの呼び出し

--- 5-3/Main.java
+++ 5-3/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-  public static {
-    
+  public static void methodA() {
+    System.out.println("methodA");
+    methodB();
+  }
+  public static void methodB() {
+    System.out.println("methodB");
+  }
+  public static void main(String[] args) {
+    methodA();
   }
 }
