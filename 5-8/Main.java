81b0755b2e3a150f0ef95e035f37332fb5e2eef8
HiromitsuMiyahara <0912.hiromi.0912@gmail.com>
2024-09-15 10:47:53 +0000

    戻り値をそのまま使う

--- 5-8/Main.java
+++ 5-8/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-  public static {
-    
+  public static int add(int x, int y) {
+    int ans = x + y;
+    return ans;
+  }
+  public static void main(String[] args) {
+    System.out.println(add(add(10, 20), add(30, 40)));
   }
 }
