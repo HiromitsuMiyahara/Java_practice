3ddf4147a788bb41b6d54d4d58427b94f19394f6
HiromitsuMiyahara <0912.hiromi.0912@gmail.com>
2024-09-16 13:23:31 +0000

    練習問題1

---
+++ chap5practice/Practice1.java
@@ -0,0 +1,15 @@
+public class Practice1 {
+  public static void introductionOneself() {
+    String name = "宮原広光";
+    int age = 29;
+    double height = 172.5;
+    char zodiac = '猪';
+    System.out.println("名前は" + name + "です。");
+    System.out.println("年齢は" + age + "です。");
+    System.out.println("身長は" + height + "cmです。");
+    System.out.println("干支は" + zodiac + "です。");
+  }
+  public static void main(String[] args) {
+    introductionOneself();
+  }
+}
