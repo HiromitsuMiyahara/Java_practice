c7f12bf548f836ee6c13c7fd7abd1e7387a769ae
HiromitsuMiyahara <0912.hiromi.0912@gmail.com>
2024-09-17 13:45:08 +0000

    練習問題4

--- chap5practice/Practice4.java
+++ chap5practice/Practice4.java
@@ -1,3 +1,16 @@
-public class Practice{
-  
+public class Practice4{
+  public static double calcTriangleArea(double bottom, double height) {
+    double ans = bottom * height / 2;
+    return ans;
+  }
+  public static double calcCircleArea(double radius) {
+    double ans = radius * radius * 3.14;
+    return ans;
+  }
+  public static void main(String[] args) {
+    double calcTriangleArea = calcTriangleArea(10.0, 5.0);
+    System.out.println("三角形の面積は" + calcTriangleArea + "cm２");
+    double calcCircleArea = calcCircleArea(5.0);
+    System.out.println("円の面積は" + calcCircleArea + "cm２");
+  }
 }
