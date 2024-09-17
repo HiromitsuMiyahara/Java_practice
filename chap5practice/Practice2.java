236a2f3b98cd6ba256aeffc2de5df8919b92ae13
HiromitsuMiyahara <0912.hiromi.0912@gmail.com>
2024-09-17 12:50:06 +0000

    練習問題2

--- chap5practice/Practice2.java
+++ chap5practice/Practice2.java
@@ -1,3 +1,10 @@
-public class Practice{
-  
+public class Practice2{
+  public static void main(String[] args) {
+    email("お誘い", "uso800@xxxx.com", "今度飲みに行きませんか");
+  }
+  public static void email(String title, String address, String text) {
+    System.out.println(address + "に、以下のメールを送信しました");
+    System.out.println("件名：" + title);
+    System.out.println("本文：" + text);
+  }
 }
