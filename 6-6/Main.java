24f38921d343e0fd60444efd13f46f5684408872
HiromitsuMiyahara <0912.hiromi.0912@gmail.com>
2024-09-22 06:18:44 +0000

    CalcLogicをcalcapp.logicsに所属させる

--- 6-6/Main.java
+++ 6-6/Main.java
@@ -1,3 +1,9 @@
-public class Main {
-  
+package calcapp.logics;
+public class CalcLogic {
+  public static int tasu(int a, int b) {
+    return (a + b);
+  }
+  public static int hiku(int a, int b) {
+    return (a - b);
+  }
 }
