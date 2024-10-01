7469993bfda92ce52d480859eb18ef08dfb70b6b
HiromitsuMiyahara <0912.hiromi.0912@gmail.com>
2024-10-01 00:31:47 +0000

    コンストラクタが定義されたHeroを生み出す

--- chap9/Hero.java
+++ chap9/Hero.java
@@ -2,6 +2,10 @@ public class Hero {
   String name;
   int hp;
   Sword sword;
+  
+  public Hero() {
+    this.hp = 100;
+  }
   public void attack() {
     System.out.println(this.name + "は" + sword.name +"で攻撃した！");
     System.out.println("敵に5ポイントのダメージをあたえた！");
